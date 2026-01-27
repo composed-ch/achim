57d615d55f7284d323536599734cd373c201fe1f
Patrick Bucher <patrick.bucher@mailbox.org>
2026-01-27 20:54:05 +0100

    WIP: started implementing Cobra scaffolding

---
+++ achim/cmd/root.go
@@ -0,0 +1,20 @@
+package achim
+
+import "github.com/spf13/cobra"
+import "fmt"
+
+var rootCmd = &cobra.Command{
+	Use:   "achim",
+	Short: "Advanced Cloud Hyperscaling Infrastructure Manager",
+	Long:  "achim manages Exoscale compute instances for education",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(args)
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ achim/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "achim/cmd"
+
+func main() {
+	cmd.Execute()
+}
