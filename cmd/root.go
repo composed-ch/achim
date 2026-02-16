9ab82e83c70f5d5020cf9f1438677c53a3a21646
Patrick Bucher <patrick.bucher@mailbox.org>
2026-02-16 15:39:13 +0100

    implemented command structure (in single file, for the moment)

--- cmd/root.go
+++ cmd/root.go
@@ -12,8 +12,31 @@ var rootCmd = &cobra.Command{
 	Short: "Advanced Cloud Hyperscaling Infrastructure Manager",
 	Long:  "achim manages Exoscale compute instances for education",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(args)
+		fmt.Println("Usage: achim [noun] [verb] (or use --help)")
 	},
+}
+
+var accessCmd = &cobra.Command{
+	Use:   "access",
+	Short: "manage API access",
+	Long:  "manage API access tokens for Exoscale",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("TODO: achim access")
+	},
+}
+
+var accessAddCmd = &cobra.Command{
+	Use:   "add",
+	Short: "add API access",
+	Long:  "add API access for Exoscale",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("TODO: achim access add")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(accessCmd)
+	accessCmd.AddCommand(accessAddCmd)
 }
 
 func Execute() {
